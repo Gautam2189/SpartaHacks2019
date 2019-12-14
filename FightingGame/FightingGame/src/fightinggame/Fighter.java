21cbc2407ad62c057b1617908eadedd0ce9077fe
ChristophG123 <58884026+ChristophG123@users.noreply.github.com>
2019-12-14 10:51:32 -0500

    Add files via upload

---
+++ FightingGame/FightingGame/src/fightinggame/Fighter.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fightinggame;
+
+public class Fighter {
+ 
+    protected int health;
+    protected String name;
+    protected int dmg;
+    
+    public Fighter(int health, String name){
+        this.health = health;
+        this.name = name;
+        
+    }
+    
+    public void attack(Fighter f, int atkNum){
+        if(atkNum == 1){
+            dmg = 5;
+            
+            
+        }else if(atkNum == 2){
+            dmg = 
+            
+        }else if(atkNum == 3){
+            
+        }else{
+            
+        }        
+        
+    }
+    
+    
+}
